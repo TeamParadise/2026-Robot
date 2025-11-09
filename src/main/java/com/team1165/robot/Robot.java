52945e9b5b8d5eb0b9e209b934885a04af1beb43
Ashton A. <uhdbits@duck.com>
2025-11-08 21:15:00 -0700

    Start working on IntelliJ settings, move code to correct location
    Start adding configurations for IntelliJ, and actually move the Java code to the correct location.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/robot/Robot.java
+++ src/main/java/com/team1165/robot/Robot.java
@@ -20,6 +20,8 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import com.team1165.robot.globalconstants.BuildConstants;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
