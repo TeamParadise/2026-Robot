951082477a19667947a9a5c2ddb41dae09d9e38f
Ashton A. <uhdbits@duck.com>
2025-12-11 18:18:08 -0700

    Start adding StateMachine methods from last season
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/util/statemachine/StateMachine.java
+++ src/main/java/com/team1165/util/statemachine/StateMachine.java
@@ -7,7 +7,12 @@
 
 package com.team1165.util.statemachine;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.Set;
+import org.littletonrobotics.junction.Logger;
 
 public abstract class StateMachine<S extends Enum<S>> extends SubsystemBase {
   /** The name of this subsystem. */
@@ -16,11 +21,47 @@ public abstract class StateMachine<S extends Enum<S>> extends SubsystemBase {
   /** The current state that the subsystem is in. */
   private S currentState;
 
-  /** The last time that a state change occured. */
+  /** Stores if the initial state has been transitioned to. */
+  private boolean initialized = false;
+
+  /** The last time that a state change occurred. */
   private double lastStateChangeTimestamp = 0.0;
 
   protected StateMachine(S initialState) {
     currentState = initialState;
     name = getName();
   }
+
+  public Command waitForState(S state) {
+    return Commands.waitUntil(() -> currentState == state);
+  }
+
+  public Command waitForStates(Set<S> states) {
+    return Commands.waitUntil(() -> states.contains(currentState));
+  }
+
+  public S getCurrentState() {
+    return currentState;
+  }
+
+  protected void setState(S newState) {
+    // Only attempt transition if the new state is not equal to the current state
+    if (newState != currentState) {
+      // Log the new current state
+      Logger.recordOutput(name + "/CurrentState", (currentState = newState).toString());
+
+      // Record the last state change time
+      lastStateChangeTimestamp = Timer.getTimestamp();
+    }
+    // Perform a transition
+    transition();
+  }
+
+  public boolean timeout(double duration) {
+    return (Timer.getTimestamp() - lastStateChangeTimestamp) > duration;
+  }
+
+  protected abstract void transition();
+
+  protected void update() {}
 }
