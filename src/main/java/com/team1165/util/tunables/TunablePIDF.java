0268a887f26eee4e680852f4b105bd30180d7e83
Ashton A. <uhdbits@duck.com>
2025-12-17 15:38:33 -0700

    Add TunablePIDF
    Add TunablePIDF for the ability to tune PID and feedforward values.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

---
+++ src/main/java/com/team1165/util/tunables/TunablePIDF.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.util.tunables;
+
+public class TunablePIDF {
+}
