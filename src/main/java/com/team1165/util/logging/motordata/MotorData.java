b8beee2edd30842979bb31f43875dfd4582f868a
Ashton A. <uhdbits@duck.com>
2025-12-30 15:45:23 -0700

    Add automatic frequency updating to MotorData
    This allows any value used in the code to automatically be swapped to a higher frequency, but it can also be circumvented as well through the method overloads.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/util/logging/motordata/MotorData.java
+++ src/main/java/com/team1165/util/logging/motordata/MotorData.java
@@ -11,48 +11,52 @@ import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 /**
- * Class that provides easy collection of most standard values collected from a motor (controller) through an IO class.
+ * Class that provides easy collection of most standard values collected from a motor (controller)
+ * through an IO class.
  */
-public class MotorData implements LoggableInputs {
-  /** The applied voltage to the motor (controller). */
-  public double appliedVolts = 0.0;
+public abstract class MotorData implements LoggableInputs {
+  /** The applied voltage to the motor. */
+  protected double appliedVolts = 0.0;
 
   /** Whether the motor (controller) is connected. Might be unreliable with a SPARK. */
-  public boolean connected = false;
+  protected boolean connected = false;
 
   /** Whether a fault is currently active on the motor (controller). */
-  public boolean faultActive = false;
+  protected boolean faultActive = false;
 
   /** String of the currently active faults on the motor (controller). */
-  public String faults = "";
+  protected String faults = "";
 
   /** The temperature of the motor. */
-  public double motorTemperatureCelsius = 0.0;
+  protected double motorTemperatureCelsius = 0.0;
 
   /**
-   * The output current of the motor. This is the general output current on a SPARK and
-   * the torque current on a Talon.
+   * The output current of the motor. This is the general output current on a SPARK and the torque
+   * current on a Talon.
    */
-  public double outputCurrentAmps = 0.0;
+  protected double outputCurrentAmps = 0.0;
 
   /**
    * The position reported by the motor (controller). Typically motor rotations, but could be
    * different depending on the specific conversion factors on the motor controller.
    */
-  public double position = 0.0;
+  protected double position = 0.0;
 
   /** The temperature of the motor controller. Not applicable on a SPARK. */
-  public double processorTemperatureCelsius = 0.0;
+  protected double processorTemperatureCelsius = 0.0;
 
   /** The supply current provided to the motor controller. */
-  public double supplyCurrentAmps = 0.0;
+  protected double supplyCurrentAmps = 0.0;
 
   /**
    * The velocity reported by the motor (controller). Typically reported in motor rotations per
    * minute, but could be different depending on the specific conversion factors on the motor
    * controller.
    */
-  public double velocity = 0.0;
+  protected double velocity = 0.0;
+
+  /** Bitmask used to raise the frequency of values that are actually used in the robot code. */
+  private int accessed = 0;
 
   /**
    * Updates a LogTable with the data to log.
@@ -85,8 +89,192 @@ public class MotorData implements LoggableInputs {
     motorTemperatureCelsius = table.get("MotorTemperatureCelsius", motorTemperatureCelsius);
     outputCurrentAmps = table.get("OutputCurrentAmps", outputCurrentAmps);
     position = table.get("Position", position);
-    processorTemperatureCelsius = table.get("ProcessorTemperatureCelsius", processorTemperatureCelsius);
+    processorTemperatureCelsius =
+        table.get("ProcessorTemperatureCelsius", processorTemperatureCelsius);
     supplyCurrentAmps = table.get("SupplyCurrentAmps", supplyCurrentAmps);
     velocity = table.get("Velocity", velocity);
+  }
+
+  /**
+   * Check if the {@link MotorField} has been accessed and update the frequency if it has not.
+   *
+   * @param field The {@link MotorField} to check.
+   */
+  private void markAccessed(MotorField field) {
+    int oldAccessed = accessed;
+    accessed |= (1 << field.ordinal());
+    if (accessed != oldAccessed) { // if the first time being accessed
+      setFrequency(field);
+    }
+  }
+
+  /**
+   * Set the frequency for the provided {@link MotorField}.
+   *
+   * @param field The {@link MotorField} to set the frequency for.
+   */
+  private void setFrequency(MotorField field) {}
+
+  /** Get the applied voltage to the motor. */
+  public double getAppliedVolts() {
+    markAccessed(MotorField.APPLIED_VOLTS);
+    return appliedVolts;
+  }
+
+  /**
+   * Get the applied voltage to the motor.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getAppliedVolts(boolean raiseFrequency) {
+    return raiseFrequency ? getAppliedVolts() : appliedVolts;
+  }
+
+  /** Get whether the motor (controller) is connected. Might be unreliable with a SPARK. */
+  public boolean getConnected() {
+    markAccessed(MotorField.CONNECTED);
+    return connected;
+  }
+
+  /**
+   * Get whether the motor (controller) is connected.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public boolean getConnected(boolean raiseFrequency) {
+    return raiseFrequency ? getConnected() : connected;
+  }
+
+  /** Get whether a fault is currently active on the motor (controller). */
+  public boolean getFaultActive() {
+    markAccessed(MotorField.FAULT_ACTIVE);
+    return faultActive;
+  }
+
+  /**
+   * Get whether a fault is currently active on the motor (controller).
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public boolean getFaultActive(boolean raiseFrequency) {
+    return raiseFrequency ? getFaultActive() : faultActive;
+  }
+
+  /** Get the string of the currently active faults on the motor (controller). */
+  public String getFaults() {
+    markAccessed(MotorField.FAULTS);
+    return faults;
+  }
+
+  /**
+   * Get the string of the currently active faults on the motor (controller).
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public String getFaults(boolean raiseFrequency) {
+    return raiseFrequency ? getFaults() : faults;
+  }
+
+  /** Get the temperature of the motor. */
+  public double getMotorTemperatureCelsius() {
+    markAccessed(MotorField.MOTOR_TEMP);
+    return motorTemperatureCelsius;
+  }
+
+  /**
+   * Get the temperature of the motor.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getMotorTemperatureCelsius(boolean raiseFrequency) {
+    return raiseFrequency ? getMotorTemperatureCelsius() : motorTemperatureCelsius;
+  }
+
+  /**
+   * Get the output current of the motor. This is the general output current on a SPARK and the
+   * torque current on a Talon.
+   */
+  public double getOutputCurrentAmps() {
+    markAccessed(MotorField.OUTPUT_CURRENT);
+    return outputCurrentAmps;
+  }
+
+  /**
+   * Get the output current of the motor. This is the general output current on a SPARK and the
+   * torque current on a Talon.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getOutputCurrentAmps(boolean raiseFrequency) {
+    return raiseFrequency ? getOutputCurrentAmps() : outputCurrentAmps;
+  }
+
+  /**
+   * Get the position reported by the motor (controller). Typically motor rotations, but could be
+   * different depending on the specific conversion factors on the motor controller.
+   */
+  public double getPosition() {
+    markAccessed(MotorField.POSITION);
+    return position;
+  }
+
+  /**
+   * Get the position reported by the motor (controller). Typically motor rotations, but could be
+   * different depending on the specific conversion factors on the motor controller.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getPosition(boolean raiseFrequency) {
+    return raiseFrequency ? getPosition() : position;
+  }
+
+  /** Get the temperature of the motor controller. Not applicable on a SPARK. */
+  public double getProcessorTemperatureCelsius() {
+    markAccessed(MotorField.PROCESSOR_TEMP);
+    return processorTemperatureCelsius;
+  }
+
+  /**
+   * Get the temperature of the motor controller. Not applicable on a SPARK.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getProcessorTemperatureCelsius(boolean raiseFrequency) {
+    return raiseFrequency ? getProcessorTemperatureCelsius() : processorTemperatureCelsius;
+  }
+
+  /** Get the supply current provided to the motor controller. */
+  public double getSupplyCurrentAmps() {
+    markAccessed(MotorField.SUPPLY_CURRENT);
+    return supplyCurrentAmps;
+  }
+
+  /**
+   * Get the supply current provided to the motor controller.
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getSupplyCurrentAmps(boolean raiseFrequency) {
+    return raiseFrequency ? getSupplyCurrentAmps() : supplyCurrentAmps;
+  }
+
+  /**
+   * Get the velocity reported by the motor (controller). Typically reported in motor rotations per
+   * minute, but could be different depending on the specific conversion factors on the motor
+   * controller.
+   */
+  public double getVelocity() {
+    markAccessed(MotorField.VELOCITY);
+    return velocity;
+  }
+
+  /**
+   * Get velocity reported by the motor (controller). Typically reported in motor rotations per
+   * minute, but could be different depending on the specific conversion factors on the motor
+   *
+   * @param raiseFrequency If true, raise the update frequency for the value.
+   */
+  public double getVelocity(boolean raiseFrequency) {
+    return raiseFrequency ? getVelocity() : velocity;
   }
 }
---
+++ src/main/java/com/team1165/util/logging/motordata/MotorField.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025-2026 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.util.logging.motordata;
+
+/** Enum representing the fields collected by {@link MotorData}. */
+enum MotorField {
+  APPLIED_VOLTS,
+  CONNECTED,
+  FAULT_ACTIVE,
+  FAULTS,
+  MOTOR_TEMP,
+  OUTPUT_CURRENT,
+  POSITION,
+  PROCESSOR_TEMP,
+  SUPPLY_CURRENT,
+  VELOCITY
+}
