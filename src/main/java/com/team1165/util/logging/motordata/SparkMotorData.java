586294fea18985daaff4125d2b078fd54c57d930
Ashton A. <uhdbits@duck.com>
2025-12-30 18:34:20 -0700

    Finish up SparkMotorData
    Add the update() method, mostly carried over from last season with a few modifications.
    
    Signed-off-by: Ashton A. <uhdbits@duck.com>

--- src/main/java/com/team1165/util/logging/motordata/SparkMotorData.java
+++ src/main/java/com/team1165/util/logging/motordata/SparkMotorData.java
@@ -7,8 +7,10 @@
 
 package com.team1165.util.logging.motordata;
 
+import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.Faults;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SignalsConfig;
@@ -19,7 +21,9 @@ import com.team1165.util.constants.CANConstants;
 import com.team1165.util.constants.CANFrequency;
 import com.team1165.util.vendor.rev.SparkConfig;
 import com.team1165.util.vendor.rev.SparkModel;
+import com.team1165.util.vendor.rev.SparkUtils;
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 
@@ -32,15 +36,18 @@ public class SparkMotorData extends MotorData {
   private final SparkBase spark;
   private final RelativeEncoder encoder;
 
-  // Model to use for configuration if updating frequency
-  private final SparkModel model;
-
   // Alerts to send if any issues arise with the SPARK or motor
   private final Alert connectedAlert;
   private final Alert faultAlert;
 
+  /** Model to use for configuration if updating frequency */
+  private final SparkModel model;
+
+  /** The applied output of the motor, used to calculate other motor fields. */
+  private double appliedOutput = 0.0;
+
   /** Debouncer to avoid false disconnection alerts. */
-  private final Debouncer connnectedDebouncer = new Debouncer(0.20);
+  private final Debouncer connectedDebouncer = new Debouncer(0.2, DebounceType.kFalling);
 
   /**
    * Creates a new {@link SparkMotorData} using the specified constants.
@@ -67,6 +74,48 @@ public class SparkMotorData extends MotorData {
             AlertConstants.general,
             "SPARK \"" + config.name() + "\" (ID: " + config.canId() + ") has active faults!",
             AlertType.kError);
+  }
+
+  /**
+   * Update the motor data using the values from the REV SPARK (MAX/FLEX) motor controller linked
+   * with this instance.
+   */
+  public void update() {
+    // Check if there are any active faults, if there are, activate an alert and save the faults
+    faultAlert.set(
+        faultActive = SparkUtils.ifOkOrDefault(spark, spark::hasActiveFault, faultActive));
+    if (faultActive) {
+      Faults sparkFaults = spark.getFaults();
+      faults =
+          (sparkFaults.other ? "other " : "")
+              + (sparkFaults.motorType ? "motorType " : "")
+              + (sparkFaults.sensor ? "sensor " : "")
+              + (sparkFaults.can ? "can " : "")
+              + (sparkFaults.temperature ? "temperature " : "")
+              + (sparkFaults.gateDriver ? "gateDriver " : "")
+              + (sparkFaults.escEeprom ? "escEeprom " : "")
+              + (sparkFaults.firmware ? "firmware " : "");
+    } else {
+      faults = "";
+    }
+
+    // Get applied output since it's used later on
+    appliedOutput = SparkUtils.ifOkOrDefault(spark, spark::getAppliedOutput, appliedOutput);
+
+    // Get values from the SPARK and save them
+    appliedVolts =
+        SparkUtils.ifOkOrDefault(spark, () -> spark.getBusVoltage() * appliedOutput, appliedVolts);
+    motorTemperatureCelsius =
+        SparkUtils.ifOkOrDefault(spark, spark::getMotorTemperature, motorTemperatureCelsius);
+    outputCurrentAmps = SparkUtils.ifOkOrDefault(spark, spark::getOutputCurrent, outputCurrentAmps);
+    position = SparkUtils.ifOkOrDefault(spark, encoder::getPosition, position);
+    processorTemperatureCelsius = 0.0; // Not compatible with SPARKs
+    supplyCurrentAmps = outputCurrentAmps * appliedOutput; // Approximate, not exact
+    velocity = SparkUtils.ifOkOrDefault(spark, encoder::getVelocity, velocity);
+
+    // Update connected debouncer if there is a sticky error from previous call to the SPARK
+    connectedAlert.set(
+        !(connected = connectedDebouncer.calculate(spark.getLastError() == REVLibError.kOk)));
   }
 
   @Override
