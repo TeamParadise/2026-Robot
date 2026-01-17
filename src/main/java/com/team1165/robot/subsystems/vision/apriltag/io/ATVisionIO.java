ceacc9eb53d1fdf94bce24d69bb57cbf71f47fe9
Hanush Patibandla <hancsh.the.1@outlook.com>
2026-01-17 11:50:24 -0700

    Added Vision code and creaated subsystems file. Also installed the new photonvision vendordeps

---
+++ src/main/java/com/team1165/robot/subsystems/vision/apriltag/io/ATVisionIO.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2025-2026 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.robot.subsystems.vision.apriltag.io;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import org.littletonrobotics.junction.AutoLog;
+
+/**
+ * A hardware interface/implementation layer for a camera that estimates the pose of the robot with
+ * AprilTags placed around the field.
+ */
+public interface ATVisionIO {
+  /**
+   * Updates a {@link ATVisionIOInputs} instance with the latest updates from this {@link
+   * ATVisionIO}.
+   *
+   * @param inputs A {@link ATVisionIOInputs} instance to update.
+   */
+  default void updateInputs(ATVisionIOInputs inputs) {}
+
+  /**
+   * Method to enable or disable single-tag pose estimation through Tx/Ty measurements. By disabling
+   * this, the standard 3D solver method will be used for single-tag estimation.
+   *
+   * @param enable Boolean that represents whether to enable or disable Tx/Ty estimation.
+   */
+  default void setSingleTagTrig(boolean enable) {}
+
+  /** Class used to store the IO values of an AprilTag vision camera. */
+  @AutoLog
+  class ATVisionIOInputs {
+    /** Value that represents if the camera is currently connected or not. */
+    public boolean connected = false;
+
+    /** The name of the camera, if it has one. Default is "N/A". */
+    public String name = "N/A";
+
+    /** Camera pose observations gathered since the last time the input was updated. */
+    public CameraPoseObservation[] poseObservations = new CameraPoseObservation[0];
+
+    /** Tx/Ty Single tag observations gathered since the last time the input was updated. */
+    public SingleTagObservation[] singleTagObservations = new SingleTagObservation[0];
+
+    /**
+     * The AprilTag IDs that have been visible at least once since the last time the input was
+     * updated.
+     */
+    public int[] tagIds = new int[0];
+  }
+
+  /**
+   * Represents the pose observation from a AprilTag camera processed on the coprocessor.
+   *
+   * @param bestCameraPose The best camera result, or the pose observation itself. Note, this is the
+   *     pose of the camera, not the robot.
+   * @param alternateCameraPose The alternative camera result, if one exists.
+   * @param averageTagDistance The average distance of the AprilTags observed from the camera.
+   * @param ambiguity The measure of how "ambiguous" (unsure/unclear) the pose observation is.
+   * @param tagCount The number of AprilTags used to make this pose observation.
+   * @param timestamp The synced timestamp of the pose observation.
+   */
+  record CameraPoseObservation(
+      Pose3d bestCameraPose,
+      Pose3d alternateCameraPose,
+      double averageTagDistance,
+      double ambiguity,
+      int tagCount,
+      double timestamp) {}
+
+  /**
+   * Represents a single tag observation from a AprilTag camera, processed alongside the IMU
+   * rotation of the robot to find the robot pose.
+   *
+   * @param tx The horizontal (yaw) rotation of the tag from the camera.
+   * @param ty The vertical (pitch) rotation of the tag from the camera.
+   * @param tagId The AprilTag ID observed in this observation.
+   * @param distance The 3D distance of the tag from the camera.
+   * @param timestamp The synced timestamp of the pose observation.
+   */
+  record SingleTagObservation(double tx, double ty, int tagId, double distance, double timestamp) {}
+}
---
+++ src/main/java/com/team1165/robot/subsystems/vision/apriltag/io/ATVisionIOPhoton.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2025-2026 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.robot.subsystems.vision.apriltag.io;
+
+import com.team1165.robot.subsystems.vision.apriltag.constants.ATVisionConstants;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N3;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.MultiTargetPNPResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
+
+/**
+ * {@link ATVisionIO} class that implements a AprilTag pose estimation camera powered by a
+ * coprocessor running PhotonVision.
+ */
+public class ATVisionIOPhoton implements ATVisionIO {
+
+  // Camera and camera intrinsics objects
+  protected final PhotonCamera camera;
+  private Matrix<N3, N3> camIntrinsics;
+  private boolean trigEnabled = false;
+
+  /**
+   * Creates a new {@link ATVisionIOPhoton} with the provided name.
+   *
+   * @param name The configured name of the camera.
+   */
+  public ATVisionIOPhoton(String name) {
+    camera = new PhotonCamera(name);
+    camIntrinsics = camera.getCameraMatrix().orElse(null);
+  }
+
+  /**
+   * Updates a {@link ATVisionIOInputs} instance with the latest updates from this {@link
+   * ATVisionIO}.
+   *
+   * @param inputs A {@link ATVisionIOInputs} instance to update.
+   */
+  @Override
+  public void updateInputs(ATVisionIOInputs inputs) {
+    // Save camera connection status to inputs object
+    inputs.connected = camera.isConnected();
+
+    // Make sure the name of the camera is saved to inputs
+    inputs.name = camera.getName();
+
+    // Save tag IDs and pose/single-tag observations to add to inputs later
+    Set<Short> tagIds = new HashSet<>();
+    Queue<CameraPoseObservation> poseObservations = new ArrayDeque<>(5);
+    Queue<SingleTagObservation> singleTagObservations = new ArrayDeque<>(5);
+
+    // Read new camera observations
+    for (var result : camera.getAllUnreadResults()) {
+      // Use MultiTag instead of single tag, if possible
+      if (result.multitagResult.isPresent()) {
+        // Get latest MultiTag result
+        MultiTargetPNPResult multitagResult = result.multitagResult.get();
+
+        // Calculate average tag distance
+        double totalTagDistance = 0.0;
+        for (var tag : result.targets) {
+          totalTagDistance += tag.bestCameraToTarget.getTranslation().getNorm();
+        }
+
+        // Add tag IDs
+        tagIds.addAll(multitagResult.fiducialIDsUsed);
+
+        // Add pose observation
+        poseObservations.add(
+            new CameraPoseObservation(
+                new Pose3d(
+                    multitagResult.estimatedPose.best.getTranslation(),
+                    multitagResult.estimatedPose.best.getRotation()), // 3D pose estimate
+                Pose3d.kZero, // MultiTag, no alternative pose
+                totalTagDistance / result.targets.size(), // Average tag distance
+                multitagResult.estimatedPose.ambiguity, // Ambiguity
+                multitagResult.fiducialIDsUsed.size(), // Tag count
+                result.getTimestampSeconds())); // Timestamp
+      } else if (!result.targets.isEmpty()) { // Check and see if there are any tags/targets at all
+        // Do a single tag calculation instead, get the tag/target
+        PhotonTrackedTarget tag = result.targets.get(0);
+
+        if (trigEnabled) { // If we are using trig, calculate tx, ty, etc.
+          // Find the center of the tag and calculate corrected value based on camera intrinsics
+          List<TargetCorner> tagCorners = tag.detectedCorners;
+          double sumX = 0.0;
+          double sumY = 0.0;
+          for (TargetCorner corner : tagCorners) {
+            sumX += corner.x;
+            sumY += corner.y;
+          }
+
+          // If camera intrinsics do exist, continue
+          if (camIntrinsics != null) {
+            double fx = camIntrinsics.get(0, 0);
+            double cx = camIntrinsics.get(0, 2);
+            double yawOffset = cx - sumX / 4;
+
+            double fy = camIntrinsics.get(1, 1);
+            double cy = camIntrinsics.get(1, 2);
+            double pitchOffset = cy - sumY / 4;
+
+            // Calculate the yaw (horizontal angle, x value)
+            var yaw = new Rotation2d(fx, yawOffset);
+            // Calculate the pitch (vertical angle, y value) with the new yaw value
+            var pitch = new Rotation2d(fy / Math.cos(Math.atan(yawOffset / fx)), -pitchOffset);
+
+            // Add tag ID
+            tagIds.add((short) tag.fiducialId);
+
+            // Add single tag observation
+            singleTagObservations.add(
+                new SingleTagObservation(
+                    yaw.getRadians(), // Yaw (horizontal angle, x value)
+                    pitch.getRadians(), // Pitch (vertical angle, y value)
+                    tag.fiducialId, // Tag ID
+                    tag.bestCameraToTarget.getTranslation().getNorm(), // Tag distance
+                    result.getTimestampSeconds())); // Timestamp
+          } else {
+            // Update camera matrix if it doesn't exist
+            camIntrinsics = camera.getCameraMatrix().orElse(null);
+          }
+        } else {
+          Optional<Pose3d> tagPose = Optional.empty();
+          if (tag.fiducialId != 14
+              && tag.fiducialId != 15
+              && tag.fiducialId != 4
+              && tag.fiducialId != 5
+              && tag.fiducialId != 3
+              && tag.fiducialId != 16) {
+            tagPose = ATVisionConstants.aprilTagLayout.getTagPose(tag.fiducialId);
+          }
+
+          // Check if the tag exists in the field layout, and if so, continue with calculation
+          if (tagPose.isPresent()) {
+            // Calculate camera pose based on the tag pose
+            Transform3d bestCameraToTarget = tag.bestCameraToTarget;
+            Pose3d bestCameraPose = tagPose.get().plus(bestCameraToTarget.inverse());
+
+            // Get alternative camera pose
+            Transform3d alternateCameraToTarget = tag.altCameraToTarget;
+            Pose3d alternateCameraPose = tagPose.get().plus(alternateCameraToTarget.inverse());
+
+            // Add tag ID
+            tagIds.add((short) tag.fiducialId);
+
+            // Add pose observation
+            poseObservations.add(
+                new CameraPoseObservation(
+                    bestCameraPose, // Best pose estimate
+                    alternateCameraPose, // Alternate pose estimate
+                    bestCameraToTarget.getTranslation().getNorm(), // Tag distance
+                    tag.poseAmbiguity, // Ambiguity
+                    1, // Tag count (one because single tag)
+                    result.getTimestampSeconds())); // Timestamp
+          }
+        }
+      }
+    }
+
+    // Save pose observations to inputs object
+    inputs.poseObservations = poseObservations.toArray(CameraPoseObservation[]::new);
+
+    // Save single tag pose
+    inputs.singleTagObservations = singleTagObservations.toArray(SingleTagObservation[]::new);
+
+    // Save tag IDs to inputs object
+    inputs.tagIds = new int[tagIds.size()];
+    int i = 0;
+    for (int id : tagIds) {
+      inputs.tagIds[i++] = id;
+    }
+  }
+
+  /**
+   * Method to enable or disable single-tag pose estimation through Tx/Ty measurements. By disabling
+   * this, the standard 3D solver method will be used for single-tag estimation.
+   *
+   * @param enable Boolean that represents whether to enable or disable Tx/Ty estimation.
+   */
+  @Override
+  public void setSingleTagTrig(boolean enable) {
+    trigEnabled = enable;
+  }
+}
