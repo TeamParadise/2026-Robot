2006febee6c2faec3f6390014af56b85bc44a90a
NT-Bean <samrahmani2010@gmail.com>
2026-01-22 15:56:18 -0700

    chore: attribution notes

---
+++ src/main/java/com/team1165/robot/subsystems/roller/spindexer/SpindexerState.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.robot.subsystems.roller.spindexer;
+
+import com.team1165.util.statemachine.v1.State;
+
+public enum SpindexerState implements State {
+  ON(),
+  OFF()
+}
