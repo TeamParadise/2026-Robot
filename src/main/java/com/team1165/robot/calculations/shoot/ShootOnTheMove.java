5b8eb7930588e81e014d7dad48644b6f9fb7d90b
NT-Bean <samrahmani2010@gmail.com>
2026-02-27 15:21:42 -0700

    added a `Vector3` class because our world has 3 dimensions. Wow! also took the `FieldConstants` class from team 6328 and added some code to `ShootOnTheMove.java`

--- src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
+++ src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
@@ -1,14 +1,25 @@
 package com.team1165.robot.calculations.shoot;
 
+import com.team1165.robot.globalconstants.FieldConstants.Hub;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.geometry.Translation3d;
 import com.team1165.robot.subsystems.drive.Drive;
 
 
 import com.team1165.robot.calculations.shoot.SpeedCalculation;
 import com.team1165.robot.calculations.Vector2;
-import com.team1165.robot.calculations.Constants;
+import com.team1165.robot.calculations.Vector3;
+import com.team1165.robot.calculations.PhysicsConstants;
+
 
 public class ShootOnTheMove {
-  public double calculate()
-  { return 1.0f; }
+  public double calculate(Drive drive, Vector2 robotSpeed, double turretHeight)
+  {
+    Vector3 turretPosition = new Vector3(drive.getPose().getX(), drive.getPose().getY(), turretHeight);
+    double turretRotation = drive.getPose().getRotation().getRadians();
+
+    Vector3 hubPosition = new Vector3(Hub.topCenterPoint.getX(), Hub.topCenterPoint.getY(), 6.0);
+
+    return 1.0f;
+  }
 }
