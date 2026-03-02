e7b24629638adedce4410caa41729784a07b946e
NT-Bean <samrahmani2010@gmail.com>
2026-03-02 16:14:05 -0700

    `ShootOnTheMove`s `calculate` function finished. it returns a `Vector2` which defines how the ball should be shot. shoutout to desmos

--- src/main/java/com/team1165/robot/calculations/Vector2.java
+++ src/main/java/com/team1165/robot/calculations/Vector2.java
@@ -20,38 +20,25 @@ public class Vector2 {
     this.y = vector.getY();
   }
 
-  public double getX() {
-    return x;
-  }
+  public double getX() { return x; }
+  public double getY() { return y; }
 
-  public double getY() {
-    return y;
-  }
+  public void setX(double x) { this.x = x; }
+  public void setY(double y) { this.y = y; }
 
-  public void setX(double x) {
-    this.x = x;
-  }
+  public double magnitude() { return Math.sqrt(x * x + y * y); }
 
-  public void setY(double y) {
-    this.y = y;
-  }
+  public double direction() { return Math.atan2(y, x); }
 
-  public double magnitude() {
-    return Math.sqrt(x * x + y * y);
-  }
+  public Vector2 normalize() { return new Vector2(x / magnitude(), y / magnitude()); }
 
-  public double direction() {
-    return Math.atan2(y, x);
-  }
 
-  public Vector2 normalize() {
-    return new Vector2(x / magnitude(), y / magnitude());
-  }
+  public Vector2 add(Vector2 v) { return new Vector2(x + v.x, y + v.y); }
 
-  public Vector2 add(Vector2 v) {
-    return new Vector2(x + v.x, y + v.y);
-  }
-
+  /** Subtracts the inputted vector from the parent of this method.
+   * @param v The vector to subtract from the first vector.
+   * @return The final difference vector
+   * */
   public Vector2 minus(Vector2 v) {
     return new Vector2(x - v.x, y - v.y);
   }
--- src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
+++ src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
@@ -4,22 +4,26 @@ import com.team1165.robot.calculations.Vector2;
 import com.team1165.robot.calculations.Vector3;
 import com.team1165.robot.globalconstants.FieldConstants.Hub;
 import com.team1165.robot.subsystems.drive.Drive;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
 
 public class ShootOnTheMove {
-  public double calculate(Drive drive, Vector2 robotSpeed, double turretHeight, double shootAngle) {
-    Vector3 turretPosition =
-        new Vector3(drive.getPose().getX(), drive.getPose().getY(), turretHeight);
+
+  /** Calculates the vector at which a ball should be shot in order to make it into the hub and returns the speed.
+   * @param drive The {@link Drive} for the robot. Used to calculate the speed and position of the bot.
+   * @return The {@link Vector2} in which the ball should be shot.
+   * */
+  public Vector2 calculate(Drive drive) {
+    Vector2 robotPosition = new Vector2(drive.getPose().getX(), drive.getPose().getY());
     double turretRotation = drive.getPose().getRotation().getRadians();
+
     Vector3 hubPosition = new Vector3(Hub.topCenterPoint.getX(), Hub.topCenterPoint.getY(), 6.0);
+    Vector2 hubPosition2D = new Vector2(hubPosition);
 
-    double angleToHub = Math.atan2(hubPosition.getY() - turretPosition.getY(), hubPosition.getX() - turretPosition.getX());
+    Vector2 robotSpeed = new Vector2(drive.getSpeeds().vxMetersPerSecond * 3.28084, drive.getSpeeds().vyMetersPerSecond * 3.28084);
+    Vector2 vecToHub = robotPosition.minus(hubPosition2D);
+    Vector2 shootVector = vecToHub.minus(robotSpeed);
 
-    double ballSpeed =
-        SpeedCalculation.calculateBallSpeed(
-            shootAngle,
-            hubPosition.getZ() - turretHeight,
-            hubPosition.minus(turretPosition).magnitude());
-
-    return 1.0f;
+    return shootVector;
   }
 }
