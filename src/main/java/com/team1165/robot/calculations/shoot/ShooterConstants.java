880a68ee78ac0faf342784bd7244ed5622028af1
NTCheese <arirahmani2021@gmail.com>
2026-03-02 17:00:50 -0700

    feat: started basics for distance to hood angle and flywheel voltage converter

---
+++ src/main/java/com/team1165/robot/calculations/shoot/ShooterConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 Team Paradise - FRC 1165 (https://github.com/TeamParadise)
+ *
+ * Use of this source code is governed by the MIT License, which can be found in the LICENSE file at
+ * the root directory of this project.
+ */
+
+package com.team1165.robot.calculations.shoot;
+
+public class ShooterConstants {
+  /** the maximum angle the hood can obtain */
+  public static final double hoodMaximumAngle = 45;
+
+  /** the minimum angle the hood can obtain */
+  public static final double hoodMinimumAngle = 28;
+
+}
