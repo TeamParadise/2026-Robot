7c91f6f5dd84eda1a19cfbc5e601e4f0bcb7a276
NT-Bean <samrahmani2010@gmail.com>
2026-03-03 14:41:50 -0700

    converted `ShootOnTheMove` to a utility class

--- src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
+++ src/main/java/com/team1165/robot/calculations/shoot/ShootOnTheMove.java
@@ -9,11 +9,13 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class ShootOnTheMove {
 
+  private ShootOnTheMove() {}
+
   /** Calculates the vector at which a ball should be shot in order to make it into the hub and returns the speed.
    * @param drive The {@link Drive} for the robot. Used to calculate the speed and position of the bot.
    * @return The {@link Vector2} in which the ball should be shot.
    * */
-  public Vector2 calculate(Drive drive) {
+  public static Vector2 calculate(Drive drive) {
     Vector2 robotPosition = new Vector2(drive.getPose().getX(), drive.getPose().getY());
     double turretRotation = drive.getPose().getRotation().getRadians();
 
