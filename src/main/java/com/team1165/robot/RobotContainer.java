223744a1dc7cc110f344aca876e7351717c5a4b0
Ashton A. <uhdbits@duck.com>
2026-04-10 15:09:42 -0700

    Update at 'Fri Apr 10 15:09:41 MST 2026'

--- src/main/java/com/team1165/robot/RobotContainer.java
+++ src/main/java/com/team1165/robot/RobotContainer.java
@@ -96,7 +96,6 @@ public class RobotContainer {
                 new DualRollerIOTalonFX(
                     FlywheelConstants.primaryMotorConfig, FlywheelConstants.secondaryMotorConfig));
         hood = new Hood(new HoodIOSpark(HoodConstants.config));
-        turret = new Turret(new TurretIO() {});
         spindexer = new Spindexer(new RollerIOSpark(SpindexerConstants.config));
         transfer = new Transfer(new RollerPIDIOSpark(TransferConstants.config));
         turret = new Turret(new TurretIOTalon(Motor.config));
